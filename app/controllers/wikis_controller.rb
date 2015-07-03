cbfa977f7349679a723d8d826a5b7ee0f4147f89
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-07-02 20:19:38 -0400

    Completed functionality for Wiki model's CRUD and views. Will need to make it pretty later on.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -24,5 +24,29 @@ class WikisController < ApplicationController
     end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
+
+    def update
+      @wiki = Wiki.find(params[:id])
+      
+      if @wiki.update_attributes(params.require(:wiki).permit(:title, :body, :private))
+        flash[:notice] = "#{@wiki.title} saved!"
+        redirect_to @wiki
+      else
+        flash[:error] = "There was a problem saving your article. Please try again."
+        render :edit
+      end
+    end
+
+    def destroy
+      @wiki = Wiki.find(params[:id])
+
+      if @wiki.destroy
+        flash[:notice] = "#{@wiki.name} has been deleted."
+        redirect_to wikis_path
+      else
+        flash[:error] = "There was a problem deleting your article. Please try again."
+      end
+    end
 end
