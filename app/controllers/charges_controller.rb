3939af16b223791bb2f79f8a9cac201f25215fa7
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-07-04 23:50:13 -0400

    Untested - User role changing for Stripe submissions

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -11,20 +11,28 @@ class ChargesController < ApplicationController
   end
 
     def create
-      customer = Stripe::Customer.create(
-        email: current_user.email,
-        card: params[:stripeToken]
-      )
 
-      charge = Stripe::Charge.create(
-        customer: customer.id,
-        amount: Amount.default,
-        description: "Private Wiki Membership - #{current_user.email}",
-        currency: "cad"
-      )
+      if current_user.role == "standard"
+        customer = Stripe::Customer.create(
+          email: current_user.email,
+          card: params[:stripeToken]
+        )
 
-      flash[:success] = "Thank you for your order! We have sent a confirmation to your email address."
-      redirect_to edit_user_registration_path
+        charge = Stripe::Charge.create(
+          customer: customer.id,
+          amount: Amount.default,
+          description: "Private Wiki Membership - #{current_user.email}",
+          currency: "cad"
+        )
+    
+        current_user.update_attributes(role: "premium")
+        flash[:success] = "Thank you for your order! We have sent a confirmation to your email address."
+        redirect_to edit_user_registration_path
+      else
+        current_user.update_attributes(role: "standard") unless current_user.role == "admin"
+        flash[:success] = "Account downgrade confirmed."
+        redirect_to edit_user_registration_path
+      end
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
