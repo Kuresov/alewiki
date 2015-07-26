7cdee559c93ad2ef7e86f3e61569c06acbdc7f3f
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-07-25 20:46:54 -0400

    Minor corrections to controller and policy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,11 +1,13 @@
 class WikiPolicy < ApplicationPolicy
 
   def leave_collaboration?
-    record.collaborators.pluck(:user_id).include? user.id
+    if user
+      record.collaborators.pluck(:user_id).include? user.id
+    end
   end
 
   def set_private?
-    user.role == "admin" || user.role == "premium"
+    user.admin? || user.premium?
   end
 
   class Scope
@@ -20,18 +22,18 @@ class WikiPolicy < ApplicationPolicy
       wikis = []
       all_wikis = scope.all
 
-      if user.admin?
+      if user.nil?
+        all_wikis.each do |wiki|
+          if wiki.private? == false
+            wikis << wiki
+          end
+        end
+      elsif user.admin?
         wikis = scope.all     #If user is admin, show them everything
       elsif user.premium?
         all_wikis.each do |wiki|
           if wiki.private == false || wiki.users.include?(user)
             wikis << wiki     #If the wiki is public, or the user is the author, or the user is a collaborator, show the wiki
-          end
-        end
-      else
-        all_wikis.each do |wiki|
-          if wiki.private? == false || wiki.users.include?(user)
-            wikis << wiki
           end
         end
       end
