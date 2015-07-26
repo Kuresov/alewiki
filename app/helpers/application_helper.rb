251fd003e6dd8662257ffca9cfe226398870cc97
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-07-26 15:32:53 -0400

    Added Stripdown support via RedCarpet for Index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
+require 'redcarpet/render_strip'
+
 module ApplicationHelper
   
   def markdown_to_html(markdown)
     renderer = Redcarpet::Render::HTML.new
     extensions = { fenced_code_blocks: true }
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
+  def markdown_to_plaintext(markdown)
+    renderer = Redcarpet::Render::StripDown.new
+    redcarpet = Redcarpet::Markdown.new(renderer)
     (redcarpet.render markdown).html_safe
   end
 end
