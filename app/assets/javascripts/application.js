ee61d09c54f6c8747cabb9de3eccd3c022c9e2d2
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-07-30 21:42:02 -0400

    EpicEditor integration on wiki#new seemingly complete, need to test tomorrow. Todo: add to edit page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,46 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require epiceditor.min.js
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+var opts = {
+  container: 'epiceditor',
+  textarea: null,
+  basePath: '../',
+  clientSideStorage: true,
+  localStorageName: 'epiceditor',
+  useNativeFullscreen: true,
+  parser: marked,
+  file: {
+    name: 'epiceditor',
+    defaultContent: '',
+    autoSave: 100
+  },
+  theme: {
+    base: 'themes/base/epiceditor.css',
+    preview: 'themes/preview/preview-dark.css',
+    editor: 'themes/editor/epic-dark.css'
+  },
+  button: {
+    preview: true,
+    fullscreen: true,
+    bar: "auto"
+  },
+  focusOnLoad: false,
+  shortcut: {
+    modifier: 18,
+    fullscreen: 70,
+    preview: 80
+  },
+  string: {
+    togglePreview: 'Toggle Preview Mode',
+    toggleEdit: 'Toggle Edit Mode',
+    toggleFullscreen: 'Enter Fullscreen'
+  },
+  autogrow: false
+}
