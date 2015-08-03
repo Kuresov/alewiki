1cb11aa4ed1b206bec3bdec8ec5d85a45d254599
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-08-03 17:01:06 -0400

    Added form validation for wikis#edit and wikis#new with a helper method

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,14 @@
 class Wiki < ActiveRecord::Base
   has_many :collaborators
   has_many :users, through: :collaborators
+  before_save :default_values
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+
+  private
+
+  def default_values
+    self.private ||= false
+  end
 end
